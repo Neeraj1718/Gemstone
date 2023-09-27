4a25f05358751f40a03a668ae2ace446babc3d61
Neeraj <neerajpal1801@gmail.com>
2023-09-27 00:30:02 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements()
+
+
+setup(
+    name='GEMSTONEPROJECT',
+    version='0.0.1',
+    author='Neeraj',
+    author_email='neerajpal1801@gmail.com',
+    install_requires=['numpy','pandas'],
+    packages=find_packages()
+
+)
---
+++ src/utils.py
@@ -0,0 +1,1 @@
+# any common functions 
